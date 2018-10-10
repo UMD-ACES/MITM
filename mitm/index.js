f292ab6caa97817a26daf5c3c398c7019ee34e27
lhmerino <louishenrimerino@hotmail.com>
2018-10-10 15:56:00 -0400

    Update index.js

--- mitm/index.js
+++ mitm/index.js
@@ -23,7 +23,7 @@ let path            = require('path'),
     crypt3          = require('crypt3/sync');
 
 let config;
-let version = 1.01
+let version = 1.2
 
 const {spawnSync} = require('child_process');
 const {execSync} = require('child_process');
