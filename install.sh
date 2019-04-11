15146e5b067b80450e7ac6e266dbefa7319c6f56
Louis-Henri Merino <louishenrimerino@hotmail.com>
2019-04-11 03:16:48 -0400

    Update Install Script to include installing latest nodejs

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-apt-get install -y sudo build-essential curl php-cli
+apt-get install -y sudo build-essential curl php-cli gcc g++ make
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
 
 cd "$(dirname "$0")"
 
