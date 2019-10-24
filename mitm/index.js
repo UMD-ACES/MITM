b591aaa975ee2c580d527c59d1455627fa2cc40a
lhmerino <lhmerino@umd.edu>
2019-10-24 22:47:13 +0200

    Update index.js

--- mitm/index.js
+++ mitm/index.js
@@ -23,7 +23,7 @@ let path            = require('path'),
     crypt3          = require('crypt3/sync');
 
 let config;
-let version = 1.3;
+let version = 1.4;
 
 const {spawnSync} = require('child_process');
 const {execSync} = require('child_process');
